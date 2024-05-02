253cbba613840f77aea86cfeecfc02d8cac3ac83
Daniel KAMINSKI <daniel.kaminski@rbinternational.com>
2024-05-02 17:00:00 +0200

    feat: new example

---
+++ ArrayExercise/Height_Checker/heightChecker.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        start = 0
+        counter = 0
+        sorting = heights.copy()
+        sorting.sort()
+        while start < len(heights):
+            if sorting[start] != heights[start]:
+                counter += 1
+            start += 1
+        return counter
